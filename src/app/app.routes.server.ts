f7c11e3274d00c3a83270ba8f3fb731d3439626c
mrbunroeun <bunroeunhas@gmail.com>
2026-09-05 11:49:29 +0700

    fix error

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,20 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'projects/:filter',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => [
+      { filter: 'all' },
+      { filter: 'angular' },
+      { filter: 'vue' },
+      { filter: 'react' },
+      { filter: 'laravel' },
+      { filter: 'fullstack' },
+      { filter: 'frontend' },
+      { filter: 'backend' }
+    ]
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
